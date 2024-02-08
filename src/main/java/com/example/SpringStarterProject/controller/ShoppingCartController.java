5ec6bdb96a0067a8412dced846979e8720df354f
suzu784 <s.suzuki784@hotmail.com>
2024-02-08 13:35:34 +0900

    modified an Exercise6

--- src/main/java/com/example/SpringStarterProject/controller/ShoppingCartController.java
+++ src/main/java/com/example/SpringStarterProject/controller/ShoppingCartController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.SpringStarterProject.model.Item;
@@ -16,6 +17,9 @@ import jakarta.servlet.http.HttpSession;
 
 /**
  * ShoppingCartController
+ * ショッピングカートのコントローラクラス.
+ * 
+ * @author suzuki
  */
 @Controller
 @RequestMapping("/exam06")
@@ -25,44 +29,46 @@ public class ShoppingCartController {
   @Autowired
   private ServletContext application;
 
-  @GetMapping("")
+  @GetMapping({ "", "/" })
   public String index(Model model) {
     if (application.getAttribute("itemList") == null) {
-    List<Item> itemList = new LinkedList<>();
+      List<Item> itemList = new LinkedList<>();
       itemList.add(new Item("手帳ノート", 1000));
       itemList.add(new Item("文房具セット", 1500));
       itemList.add(new Item("ファイル", 2000));
       application.setAttribute("itemList", itemList);
     }
 
+    int total = 0;
+    model.addAttribute("total", total);
     if (session.getAttribute("cartItems") == null) {
       List<Item> cartItems = new LinkedList<>();
       session.setAttribute("cartItems", cartItems);
+    } else {
+      List<Item> itemList = (List<Item>) session.getAttribute("cartItems");
+      for (Item item : itemList) {
+        total += item.getPrice();
+      }
+      model.addAttribute("total", total);
     }
 
-    List<Item> itemList = (List<Item>) application.getAttribute("itemList");
-    int total = 0;
-    for (Item item : itemList) {
-      total += item.getPrice();
-    }
-    model.addAttribute("total", total);
     return "item-and-cart";
   }
 
-  @GetMapping({"/in-cart", "/in-cart/"})
-  public String inCart() {
+  @PostMapping({ "/in-cart", "/in-cart/" })
+  public String inCart(Integer item_index) {
     LinkedList<Item> itemList = (LinkedList<Item>) application.getAttribute("itemList");
-    Item item = itemList.get(0); // todo...indexを送る方法について調べる
+    Item item = itemList.get(item_index);
     LinkedList<Item> cartItems = (LinkedList<Item>) session.getAttribute("cartItems");
     cartItems.add(item);
     session.setAttribute("cartItems", cartItems);
     return "redirect:/exam06";
   }
 
-  @GetMapping("/delete")
-  public String delete() {
+  @PostMapping({"/delete", "/delete/"})
+  public String delete(Integer item_index) {
     LinkedList<Item> cartItems = (LinkedList<Item>) session.getAttribute("cartItems");
-    cartItems.remove(0);
+    cartItems.remove(cartItems.get(item_index));
     session.setAttribute("cartItems", cartItems);
     return "redirect:/exam06";
   }
